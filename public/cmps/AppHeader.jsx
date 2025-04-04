6922968d1cd5961be288f7fb96a81bb469f57188
eliad <eliad.haim@gmail.com>
2025-04-04 07:01:14 +0300

    Improve home page

--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -29,41 +29,42 @@ export function AppHeader({ loggedinUser, setLoggedinUser }) {
     }
 
     return <header className="app-header main-content single-row">
-        <Link to="/" onClick={onCloseNav}>
-            <div className="main-logo">
-                <img src="/assets/img/bug.png" className="bug-img-logo" />
-                <span>Miss Bug</span>
-            </div>
-        </Link>
+        <div className="header-content">
+            <Link to="/" onClick={onCloseNav}>
+                <div className="main-logo">
+                    <img src="/assets/img/bug.png" className="bug-img-logo" />
+                    <span>Miss Bug</span>
+                </div>
+            </Link>
 
-        <nav className={isMenuOpen ? 'active' : ''}>
-            <div>
-                <NavLink to="/" onClick={onCloseNav}>Home</NavLink>
-                <NavLink to="/bug" onClick={onCloseNav}>Bugs</NavLink>
-                <NavLink to="/about" onClick={onCloseNav}>About</NavLink>
-                {loggedinUser && loggedinUser.isAdmin && <NavLink to="/user" onClick={onCloseNav}>users</NavLink>}
-            </div>
-        </nav>
+            <nav className={isMenuOpen ? 'active' : ''}>
+                <div>
+                    <NavLink to="/" onClick={onCloseNav}>Home</NavLink>
+                    <NavLink to="/bug" onClick={onCloseNav}>Bugs</NavLink>
+                    <NavLink to="/about" onClick={onCloseNav}>About</NavLink>
+                    {loggedinUser && loggedinUser.isAdmin && <NavLink to="/user" onClick={onCloseNav}>users</NavLink>}
+                </div>
+            </nav>
 
-        <div className='login'>
-            {loggedinUser
-                ? <React.Fragment>
-                    <NavLink to={`/user/${loggedinUser._id}`}>{loggedinUser.fullname}</NavLink>
-                    <button onClick={onLogout}>logout</button>
-                </React.Fragment>
-                : <NavLink to="/auth" onClick={onCloseNav}>login</NavLink>
-            }
+            <div className='login'>
+                {loggedinUser
+                    ? <React.Fragment>
+                        <NavLink to={`/user/${loggedinUser._id}`}>{loggedinUser.fullname}</NavLink>
+                        <button onClick={onLogout}>logout</button>
+                    </React.Fragment>
+                    : <NavLink to="/auth" onClick={onCloseNav}><button>Login</button></NavLink>
+                }
+            </div>
+
+            <button className="menu-btn" onClick={() => (setIsMenuOpen(!isMenuOpen))}>
+                {isMenuOpen ?
+                    <img src="/assets/img/x.png" />
+                    :
+                    <img src="/assets/img/bars.png" />
+                }
+            </button>
+
         </div>
-
-        <button className="menu-btn" onClick={() => (setIsMenuOpen(!isMenuOpen))}>
-            {isMenuOpen ?
-                <img src="/assets/img/x.png" />
-                :
-                <img src="/assets/img/bars.png" />
-            }
-        </button>
-
-
 
     </header>
 }
