ef679d3d63325540fe13d9fa98b6ee580069e790
eliad <eliad.haim@gmail.com>
2025-04-07 00:45:48 +0300

    Improve users index style

--- public/cmps/UserList.jsx
+++ public/cmps/UserList.jsx
@@ -5,7 +5,9 @@ export function UserList({ users, onRemoveUser }) {
         <ul className="user-list">
             {users.map(user => {
                 return <li key={user._id}>
-                    <span>{user.username}</span>
+                    <div><span>Username:</span> {user.username}</div>
+                    <div><span>Fullname:</span> {user.fullname}</div>
+                    <div><span>Id:</span> {user._id}</div>
                     <button onClick={() => { onRemoveUser(user._id) }}>Remove User</button>
                 </li>
             })}
